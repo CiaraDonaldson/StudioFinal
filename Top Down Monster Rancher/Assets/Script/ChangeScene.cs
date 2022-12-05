be55056e9a70b5f1f69497244891223468495f55
CiaraDonaldson <94332296+CiaraDonaldson@users.noreply.github.com>
2022-12-05 00:29:37 -0500

    Revert "tutorial"
    This reverts commit 5e53d516057b14d2e4fc9a25dcb6e54cd85d16e8.

---
+++ Top Down Monster Rancher/Assets/Script/ChangeScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    void OnEnable()
+    {
+        SceneManager.LoadScene("Your Farm", LoadSceneMode.Additive);
+    }
+}
